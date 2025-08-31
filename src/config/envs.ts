b04685669a18d6d6a4aaa96158a8a3a1ab42c82d
Epistle-star <onunworebube3@gmail.com>
2025-09-01 00:11:51 +0100

    made some build corrections 05

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,4 @@ dotenv.config({ path: ".env.local" });
 
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || "";
-export const DATABASE_URL = process.env.DATABASE_URL || "";
+export const DATABASE_URL = process.env.DATABASE_URL;
