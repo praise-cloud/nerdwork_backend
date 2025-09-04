7905819fbb6fa6774ec2bb1e23737ef250634823
Onunwor Ebube <onunworebube3@gmail.com>
2025-09-04 02:55:49 +0100

    deployments 14

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -34,33 +34,33 @@ export const googleAuthController = async (req, res) => {
     const { email, sub: googleId, picture } = payload;
 
     // ✅ Check if user already exists
-    const users = await db
-      .select()
-      .from(authUsers)
-      .where(eq(authUsers.email, email));
-    const existingUser = users[0] ?? null;
+    // const users = await db
+    //   .select()
+    //   .from(authUsers)
+    //   .where(eq(authUsers.email, email));
+    // const existingUser = users[0] ?? null;
 
     let user;
     let isNewUser = false;
 
-    if (existingUser) {
-      user = existingUser;
-    } else {
-      // ✅ Create new user
-      const [newUser] = await db
-        .insert(authUsers)
-        .values({
-          email,
-          username: email.split("@")[0],
-          passwordHash: "secret",
-          emailVerified: true,
-          isActive: true,
-        })
-        .returning();
+    // if (existingUser) {
+    //   user = existingUser;
+    // } else {
+    // ✅ Create new user
+    const [newUser] = await db
+      .insert(authUsers)
+      .values({
+        email,
+        username: email.split("@")[0],
+        passwordHash: "secret",
+        emailVerified: true,
+        isActive: true,
+      })
+      .returning();
 
-      user = newUser;
-      isNewUser = true;
-    }
+    user = newUser;
+    isNewUser = true;
+    // }
 
     // ✅ Generate JWT
     const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
