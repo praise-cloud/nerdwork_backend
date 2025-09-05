417a8b6fda19927ecb0179cbd5e0a2df986ca2ee
Onunwor Ebube <onunworebube3@gmail.com>
2025-09-05 20:47:27 +0100

    added comic logic 06

--- src/routes/comic.routes.ts
+++ src/routes/comic.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import {
   createComic,
-  fetchAllComicByJwt,
+  // fetchAllComicByJwt,
   fetchAllComics,
   fetchComicBySlug,
 } from "../controller/comic.controller";
